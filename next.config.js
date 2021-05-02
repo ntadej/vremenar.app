fcab997b20c19f807cec87f46233e0905a9cb53b
Tadej Novak <tadej@tano.si>
2021-05-02 16:45:56 +0200

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withTM = require('next-transpile-modules')(['@tano/common'])
 
 module.exports = withTM(
   nextTranslate({
+    future: {
+      webpack5: true,
+    },
     sassOptions: {
       includePaths: [path.join(__dirname, 'assets'), path.join(__dirname, 'style')],
     },
