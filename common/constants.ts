453fc0e2513dca025148ac1881155d0bc3f49863
Tadej Novak <tadej@tano.si>
2021-07-21 15:37:43 +0200

    Release 0.3.4

--- common/constants.ts
+++ common/constants.ts
@@ -1,6 +1,6 @@
 const stableVersion = null
 const stableBuild = null
-const testingVersion = '0.3.3'
-const testingBuild = '239'
+const testingVersion = '0.3.4'
+const testingBuild = '243'
 
 export { stableVersion, stableBuild, testingVersion, testingBuild }
