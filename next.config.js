bb80a9f30a84d8f5004f0feaa82843fab2485c16
Tadej Novak <tadej@tano.si>
2022-02-11 22:09:18 +0100

    Streamline the download page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withTM = require('next-transpile-modules')(['@ntadej/style'])
 /** @type {import('next').NextConfig} */
 module.exports = withTM(
   nextTranslate({
+    images: {
+      imageSizes: [16, 32, 48, 64, 96, 128, 174, 224, 256, 348, 384, 448, 512],
+    },
     sassOptions: {
       includePaths: [path.join(__dirname, 'assets'), path.join(__dirname, 'style')],
     },
--- pages/download.tsx
+++ pages/download.tsx
@@ -1,129 +1,199 @@
-/* eslint-disable @next/next/no-img-element */
 import { NextPage } from 'next'
 import { Section } from '@ntadej/style'
 import { testingVersion, testingBuild } from 'common/constants'
+import Image from 'next/image'
 import Layout from 'components/Layout'
+import Trans from 'next-translate/Trans'
 import useTranslation from 'next-translate/useTranslation'
+
+import amazonAppStoreBadge_en from '../resources/badges/amazon-appstore_en.png'
+import amazonAppStoreBadge_de from '../resources/badges/amazon-appstore_de.png'
+
+import appStoreBadge_en from '../resources/badges/apple-app-store_en.svg'
+import appStoreBadge_de from '../resources/badges/apple-app-store_de.svg'
+import appStoreBadge_sl from '../resources/badges/apple-app-store_sl.svg'
+
+import flathubBadge_en from '../resources/badges/flathub_en.svg'
+
+import githubBadge_en from '../resources/badges/github_en.svg'
+import githubBadge_de from '../resources/badges/github_de.svg'
+import githubBadge_sl from '../resources/badges/github_sl.svg'
+
+import googlePlayBadge_en from '../resources/badges/google-play_en.png'
+import googlePlayBadge_de from '../resources/badges/google-play_de.png'
+import googlePlayBadge_sl from '../resources/badges/google-play_sl.png'
+
+import macAppStoreBadge_en from '../resources/badges/apple-mac-app-store_en.svg'
+import macAppStoreBadge_de from '../resources/badges/apple-mac-app-store_de.svg'
+import macAppStoreBadge_sl from '../resources/badges/apple-mac-app-store_sl.svg'
 
 const Download: NextPage = () => {
   const { t, lang } = useTranslation('common')
 
+  const macAppStoreBadge: { [id: string]: any } = {
+    en: macAppStoreBadge_en,
+    de: macAppStoreBadge_de,
+    sl: macAppStoreBadge_sl,
+  }
+
+  const flathubBadge: { [id: string]: any } = {
+    en: flathubBadge_en,
+    de: flathubBadge_en,
+    sl: flathubBadge_en,
+  }
+
+  const appStoreBadge: { [id: string]: any } = {
+    en: appStoreBadge_en,
+    de: appStoreBadge_de,
+    sl: appStoreBadge_sl,
+  }
+
+  const googlePlayBadge: { [id: string]: StaticImageData } = {
+    en: googlePlayBadge_en,
+    de: googlePlayBadge_de,
+    sl: googlePlayBadge_sl,
+  }
+
+  const amazonAppStoreBadge: { [id: string]: StaticImageData } = {
+    en: amazonAppStoreBadge_en,
+    de: amazonAppStoreBadge_de,
+    sl: amazonAppStoreBadge_en,
+  }
+
+  const githubBadge: { [id: string]: any } = {
+    en: githubBadge_en,
+    de: githubBadge_de,
+    sl: githubBadge_sl,
+  }
+
+  const AvailableAlsoOnGithub = (
+    <p className="narrow">
+      <Trans
+        i18nKey="download:also-on"
+        components={{
+          a: (
+            <a
+              href={`https://github.com/ntadej/Vremenar/releases/download/v${testingVersion}`}
+            />
+          ),
+        }}
+        values={{
+          site: t('download:also-on-github'),
+        }}
+      />
+    </p>
+  )
+
   return (
     <Layout title={t('download')}>
       <Section
-        title={t('download') + ' Vremenar v' + testingVersion}
+        title={t('download:with-version', { version: testingVersion })}
         subtitle={t('download:platform')}
       >
         <div className="columns">
           <div className="column is-one-third has-text-centered">
             <h2>macOS</h2>
-            <p className="narrow">
-              <a
-                href={`https://github.com/ntadej/Vremenar/releases/download/v${testingVersion}/Vremenar_${testingVersion}.dmg`}
-              >
-                <img
-                  alt="Get it on GitHub"
-                  src="/assets/misc/get-it-on-github.png"
-                  className="badge google first"
+            <p className="narrow badge">
+              <a href="">
+                <Image
+                  alt={t('download:badge-mac-app-store')}
+                  src={macAppStoreBadge[lang]}
+                  // original 156x40
+                  width="224"
+                  height="58"
                 />
               </a>
             </p>
+            {AvailableAlsoOnGithub}
           </div>
           <div className="column is-one-third has-text-centered">
             <h2>Linux</h2>
-            <p className="narrow">
+            <p className="narrow badge">
               <a href="https://flathub.org/apps/details/si.tano.Vremenar">
-                <img
-                  alt="Download on Flathub"
-                  src="https://flathub.org/assets/badges/flathub-badge-en.png"
-                  className="badge"
+                <Image
+                  alt={t('download:badge-flathub')}
+                  src={flathubBadge[lang]}
+                  // original 600x200
+                  width="224"
+                  height="75"
                 />
               </a>
             </p>
-            <p className="narrow">
-              <a
-                href={`https://github.com/ntadej/Vremenar/releases/v${testingVersion}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  alt="Get it on GitHub"
-                  src="/assets/misc/get-it-on-github.png"
-                  className="badge google"
-                />
-              </a>
-            </p>
+            {AvailableAlsoOnGithub}
           </div>
           <div className="column is-one-third has-text-centered">
             <h2>Windows</h2>
-            <p>{t('download:soon')}</p>
+            <p className="narrow badge">
+              <a href="">
+                <Image
+                  alt={t('download:badge-github')}
+                  src={githubBadge[lang]}
+                  // original 564x168
+                  width="224"
+                  height="67"
+                />
+              </a>
+            </p>
           </div>
         </div>
         <div className="columns">
           <div className="column is-one-third has-text-centered">
+            <h2>iOS</h2>
+            <p className="narrow badge">
+              <a href="">
+                <Image
+                  alt={t('download:badge-app-store')}
+                  src={appStoreBadge[lang]}
+                  // original 120x40
+                  width="174"
+                  height="58"
+                />
+              </a>
+            </p>
+          </div>
+          <div className="column is-one-third has-text-centered">
             <h2>Android &amp; Android TV</h2>
-            <p className="narrow">
+            <p className="narrow badge google">
               <a
                 href={`https://play.google.com/store/apps/details?id=si.tano.Vremenar&hl=${lang}&utm_source=Website&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1`}
               >
-                <img
-                  alt="Get it on Google Play"
-                  src={`https://play.google.com/intl/en_us/badges/static/images/badges/${lang}_badge_web_generic.png`}
-                  className="badge google first"
+                <Image
+                  alt={t('download:badge-google-play')}
+                  src={googlePlayBadge[lang]}
+                  // original 646x250
+                  width="256"
+                  height="100"
                 />
               </a>
             </p>
-            <p className="narrow">
+            <p className="narrow badge google">
               <a href="https://www.amazon.com/gp/product/B093Z72DBC/ref=mas_pm_vremenar_weather">
-                <img
-                  alt="Available at Amazon Appstore"
-                  src="https://images-na.ssl-images-amazon.com/images/G/01/mobile-apps/devportal2/res/images/amazon-appstore-badge-english-black.png"
-                  className="badge"
+                <Image
+                  alt={t('download:badge-amazon-app-store')}
+                  src={amazonAppStoreBadge[lang]}
+                  // original 572x168
+                  width="224"
+                  height="66"
                 />
               </a>
             </p>
-            <p className="narrow">
-              <a
-                href={`https://github.com/ntadej/Vremenar/releases/download/v${testingVersion}/Vremenar_${testingVersion}_${testingBuild}.apk`}
-              >
-                <img
-                  alt="Get it on GitHub"
-                  src="/assets/misc/get-it-on-github.png"
-                  className="badge google"
-                />
-              </a>
-            </p>
-          </div>
-          <div className="column is-one-third has-text-centered">
-            <h2>iOS</h2>
-            <p className="narrow">
-              <a
-                href={`https://github.com/ntadej/Vremenar/releases/download/v${testingVersion}/Vremenar_${testingVersion}.ipa`}
-              >
-                <img
-                  alt="Get it on GitHub"
-                  src="/assets/misc/get-it-on-github.png"
-                  className="badge google first"
-                />
-              </a>
-            </p>
-            <p>
-              {t('download:ios-sideload')} {t('download:ios-sideload-is-recommended')}{' '}
-              <a href="https://altstore.io/" target="_blank" rel="noreferrer">
-                AltStore
-              </a>
-              .
-            </p>
+            {AvailableAlsoOnGithub}
           </div>
           <div className="column is-one-third has-text-centered">
             <h2>{t('download:all')}</h2>
-            <a href={`https://github.com/ntadej/Vremenar/releases/v${testingVersion}`}>
-              <img
-                alt="Get it on GitHub"
-                src="/assets/misc/get-it-on-github.png"
-                className="badge google first"
-              />
-            </a>
+            <p className="narrow badge">
+              <a
+                href={`https://github.com/ntadej/Vremenar/releases/v${testingVersion}`}
+              >
+                <Image
+                  alt={t('download:badge-github')}
+                  src={githubBadge[lang]}
+                  // original 564x168
+                  width="224"
+                  height="67"
+                />
+              </a>
+            </p>
           </div>
         </div>
       </Section>
