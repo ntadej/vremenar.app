94e908aa88d8e2cd949cb96de0fd60e5f35f3811
Tadej Novak <tadej@tano.si>
2023-01-28 19:06:44 +0100

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
+
 import 'style/main.sass'
 
-const MainApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const MainApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
+)
 
 export default MainApp
